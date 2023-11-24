a533b31e57fb52aa4bcae90a9aa968e419e56513
Netrobe Ubuntu <netrobepy@gmail.com>
2023-11-24 12:09:33 +0100

    Added channels

--- api/config/asgi.py
+++ api/config/asgi.py
@@ -1,16 +1,13 @@
-"""
-ASGI config for api project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from decouple import config
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      config('DJANGO_SETTINGS_MODULE'))
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+})
