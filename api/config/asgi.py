88ee0f699b8c6d85e52e3fdbc8efd0d875f0ed4b
Netrobe Ubuntu <netrobepy@gmail.com>
2023-11-29 09:26:09 +0100

    Updated config

--- api/config/asgi.py
+++ api/config/asgi.py
@@ -8,10 +8,10 @@ from decouple import config
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       config('DJANGO_SETTINGS_MODULE'))
+django_asgi_app = get_asgi_application()
 
 from chat import routing
 
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
